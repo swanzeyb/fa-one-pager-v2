c1873502764fd9eababd40ff5af2511c856a4b35
Benjamin Swanzey <swanzeyb2001@gmail.com>
2025-07-21 00:34:48 -0700

    maybe not needed

--- hooks/index.ts
+++
@@ -1,6 +0,0 @@
-// Export convenience hooks for easy importing
-export { useFiles } from './use-files'
-export { useOutputs } from './use-outputs'
-export { useStepTracker } from './use-step-tracker'
-export { useIsMobile } from './use-mobile'
-export { useClientAI } from './use-client-ai'
--- services/index.ts
+++
@@ -1,9 +0,0 @@
-// Export all services
-export * from './file-service'
-export * from './ai-service'
-export * from './analytics-service'
-export { 
-  processOutput as processOutputWithFirebase,
-  processOutputWithFirebase as processOutputFirebase,
-  retryFirebaseAI
-} from './firebase-ai-service'
